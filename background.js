fc178d62036fd88b9110452ce0e2c412a9545db5
Nathan974-creator <113228775+Nathan974-creator@users.noreply.github.com>
2024-01-12 11:48:24 +0400

    Mettre à jour background.js avec mes modifications

---
+++ background.js
@@ -0,0 +1,53 @@
+let selector_to_delete = ['body > tp-yt-iron-overlay-backdrop', 'body > ytd-app > ytd-popup-container > tp-yt-paper-dialog']
+async function main() {
+    while (true) {
+        const activeTab = await getCurrentTab();
+        console.log(activeTab)
+        if (activeTab === undefined) {
+            await new Promise(resolve => setTimeout(resolve, 100));
+            continue
+        }
+        if (activeTab.url.includes("youtu")) {
+            await new Promise(resolve => setTimeout(resolve, 200));
+            chrome.scripting.executeScript({
+                target: { tabId: activeTab.id },
+                function: (/*arguments*/) => {
+                    console.log("avant")
+                    
+                    // Obtenez une référence vers l'élément que vous souhaitez "cliquer"
+                    var myElement = document.getElementsByClassName("ytp-ad-skip-button-modern")[0];
+                    if (myElement !== undefined) {
+                        // Créez un nouvel événement de clic
+                        var clickEvent = new Event('click', {
+                            bubbles: true
+                        });
+
+                        // Dispatchez l'événement sur l'élément
+                        myElement.dispatchEvent(clickEvent);
+                        console.log("apres")
+                    }
+
+                    var blockagePopup = document.querySelector(selector_to_delete[1])
+                    if (blockagePopup !== undefined) {
+                        blockagePopup.remove()
+                        var bgBlockagePopup = document.querySelector(selector_to_delete[0])
+                        bgBlockagePopup.remove()
+                    }
+                },
+            });
+        } else {
+            console.log("not on youtube")
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+    }
+}
+
+async function getCurrentTab() {
+    return new Promise(resolve => {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            resolve(tabs[0]);
+        });
+    });
+}
+
+main()
